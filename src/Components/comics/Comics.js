4933c0c56b2b259e34a6c7b2119afc99d23f5183
Danilo <danilorb20099@hotmail.com>
2021-02-15 21:33:39 -0600

    split code in comic component

--- src/Components/comics/Comics.js
+++ src/Components/comics/Comics.js
@@ -1,17 +1,19 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, lazy, Suspense } from 'react';
 import { getAllComics, loadingComics } from '../../Actions/ComicsActions';
 import { useDispatch, useSelector } from 'react-redux';
 import { Empty, Pagination } from 'antd';
-import Card from '../card/Card';
 import History from '../history/History';
 import LoadingView from '../loadingView/LoadingView';
 import ErrorPage from '../errorPage/ErrorPage';
-import ComicsFilters from '../comicsFilters/ComicsFilters';
+
 import { generateQueryParams } from '../utils/Utils';
 
 import { ContentWrapper, Footer } from '../globalStyles/Index';
 
 const Comics = () => {
+	const Card = lazy(() => import('../card/Card'));
+	const ComicsFilters = lazy(() => import('../comicsFilters/ComicsFilters'));
+
 	const [filters, setFilters] = useState({
 		format: '',
 		titleStartsWith: '',
@@ -38,38 +40,41 @@ const Comics = () => {
 		dispatch(loadingComics());
 		dispatch(getAllComics(generateQueryParams(filters), newOffset));
 	};
+	const renderLoader = () => <LoadingView />;
 
 	return (
 		<ContentWrapper>
-			<ComicsFilters filters={filters} setFilters={setFilters} />
-			{loading && <LoadingView />}
-			{listOfComics?.results?.length === 0 && <Empty />}
-			{error.code && (
-				<ErrorPage code={error.code} message={error.message} />
-			)}
-			{!loading &&
-				listOfComics?.results?.length > 0 &&
-				listOfComics?.results?.map((value) => (
-					<Card
-						key={value.id}
-						background={value.thumbnail.path}
-						title={value.title}
-						extension={value.thumbnail.extension}
-						onClick={() => handleOnClick(value.id)}
-						height={'400px'}
-					/>
-				))}
-			{listOfComics?.results?.length > 0 && (
-				<Footer>
-					<Pagination
-						defaultCurrent={1}
-						total={total || 0 / 20}
-						simple={true}
-						onChange={handlePagination}
-						defaultPageSize={20}
-					/>
-				</Footer>
-			)}
+			<Suspense fallback={renderLoader()}>
+				<ComicsFilters filters={filters} setFilters={setFilters} />
+				{loading && <LoadingView />}
+				{listOfComics?.results?.length === 0 && <Empty />}
+				{error.code && (
+					<ErrorPage code={error.code} message={error.message} />
+				)}
+				{!loading &&
+					listOfComics?.results?.length > 0 &&
+					listOfComics?.results?.map((value) => (
+						<Card
+							key={value.id}
+							background={value.thumbnail.path}
+							title={value.title}
+							extension={value.thumbnail.extension}
+							onClick={() => handleOnClick(value.id)}
+							height={'400px'}
+						/>
+					))}
+				{listOfComics?.results?.length > 0 && (
+					<Footer>
+						<Pagination
+							defaultCurrent={1}
+							total={total || 0 / 20}
+							simple={true}
+							onChange={handlePagination}
+							defaultPageSize={20}
+						/>
+					</Footer>
+				)}
+			</Suspense>
 		</ContentWrapper>
 	);
 };
