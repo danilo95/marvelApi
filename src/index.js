8a4edb518504ef1a78995456e05cf1bd6f863f7a
Danilo <danilorb20099@hotmail.com>
2021-02-15 22:41:46 -0600

    add logo animation

--- src/index.js
+++ src/index.js
@@ -6,17 +6,24 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { Router } from 'react-router-dom';
 import History from './Components/history/History';
+import applaudoLogo from './Assets/applaudoLogo.png';
 import reportWebVitals from './reportWebVitals';
 
 import reducers from './Reducers/Index';
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
 
+const renderLoader = () => (
+	<div class="full-page-loader">
+		<img width="200" src={applaudoLogo} alt="applaudo-logo" />
+	</div>
+);
+
 ReactDOM.render(
 	<React.StrictMode>
 		<Provider store={store}>
 			<Router history={History}>
-				<Suspense fallback={<span>Loading</span>}>
+				<Suspense fallback={renderLoader()}>
 					<App />
 				</Suspense>
 			</Router>
